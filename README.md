c41eb8a2a7484db4bd262bfbdce846478d3dbf88
Helen <liaforweb@gmail.com>
2019-08-30 14:05:21 +0300

    contacts desc

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# 2019-08-kvizz
+# 2019-07-psychologist
+
+<a href="https://lia5.github.io/2019-08-kviz/myapp/index.html">Главная</a>
+<a href="https://lia5.github.io/2019-08-kviz/myapp/contacts.html">Контакты</a>
---
+++ myapp/assets/js/custom.js
@@ -0,0 +1,91 @@
+$(function() {
+
+//slider bio
+if(jQuery('.bio__slider').length) {
+    $('.bio__slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: false,
+      arrows: true,
+      dots: false,
+      prevArrow: '<div class="arrow-prev"></div>',
+      nextArrow: '<div class="arrow-next"></div>'
+    });
+}
+
+// MAP
+    
+ymaps.ready(init);
+function init(){     
+   var myMap = new ymaps.Map("map", {
+        center: [55.752156, 37.588548],
+        zoom: 15
+    }),
+    
+    // Создаем метку с помощью вспомогательного класса.
+    myPlacemark1 = new ymaps.Placemark([55.752156, 37.588548], {
+     // Свойства.
+     // Содержимое иконки, балуна и хинта.
+     balloonContentBody: 'Квиzzов',
+    balloonContentFooter: 'БЦ «Восток» ул. Новый Арбат, 19',
+    hintContent: 'Квиzzов'
+ }, {
+     // Опции.
+     // Своё изображение иконки метки.
+    //  iconLayout: 'default#imageWithContent',
+    //  iconImageHref: 'img/icons/marker.png',
+     // Размеры метки.
+    //  iconImageSize: [66, 100]
+ });
+
+// Добавляем все метки на карту.
+myMap.geoObjects.add(myPlacemark1);
+myMap.behaviors.disable('scrollZoom');
+
+};
+ // MAP
+
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+//menu
+    var menu = document.querySelector('.menu-toggle');
+    menu.addEventListener('click', function(){
+        var nav = document.querySelector('.main-menu');
+        nav.classList.toggle('active');
+        var navGamb = document.querySelector('.menu-toggle');
+        navGamb.classList.toggle('active');
+    });
+//tabs
+	// store tabs variable
+	var myTabs = document.querySelectorAll("ul.header__tabs > li");
+    function myTabClicks(tabClickEvent) {
+		for (var i = 0; i < myTabs.length; i++) {
+			myTabs[i].classList.remove("active");
+		}
+		var clickedTab = tabClickEvent.currentTarget;
+		clickedTab.classList.add("active");
+		tabClickEvent.preventDefault();
+		var myContentPanes = document.querySelectorAll(".tab-pane");
+		for (i = 0; i < myContentPanes.length; i++) {
+			myContentPanes[i].classList.remove("active");
+		}
+        var anchorReference = tabClickEvent.target;
+        console.log(anchorReference);
+        var activePaneId = anchorReference.getAttribute("href");
+        console.log(activePaneId);
+        var activePane = document.querySelector(activePaneId);
+        console.log(activePaneId);
+		activePane.classList.add("active");
+    }
+    for (i = 0; i < myTabs.length; i++) {
+		myTabs[i].addEventListener("click", myTabClicks)
+	}
+
+
+
+
+
+});
